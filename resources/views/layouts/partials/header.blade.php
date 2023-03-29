77c001e4f27e1a662720117a22e81acb0c6ba721
orkit <ryan@dias.se>
2023-03-29 11:02:51 +0200

    Modified logo

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,10 +1,28 @@
+<style>
+    .logo h1 {
+        font-family: "Raleway", sans-serif;
+        font-size: 36px;
+        margin: 0;
+        padding: 0;
+        line-height: 1;
+    }
+    .logo h1 span {
+        color: #3ec1d5;
+    }
+    .logo h1 a,
+    .logo h1 a:hover {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
 <div id="top-spacer" style="height: 110px;"></div>
 <header aria-label="Sidhuvud" id="main-header" class="su-header-container__primary fixed-top">
     <!-- Navigation -->
     <div class="container d-flex h-100">
-        <a href="#" class="text-decoration-none logo d-sm-flex align-items-center">
-            DSV Workflow Management
-        </a>
+        <div class="text-decoration-none logo d-sm-flex align-items-center">
+            <h1><a href="#"><span>DSV</span>WorkFlow</a></h1>
+        </div>
+
     </div>
 </header>
 
