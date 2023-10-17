28ce086e4eceb2910f90748277229d97962a99eb
orkit <ryan@dias.se>
2023-10-17 15:32:57 +0200

    Fixed order

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -336,7 +336,7 @@
                        OTRS
                 </span>
             </a>
-            <a href="https://play.dsv.su.se" class="md:order-7 p-4 relative flex flex-col justify-center items-center md:min-h-fit
+            <a href="https://play.dsv.su.se" class="md:order-8 p-4 relative flex flex-col justify-center items-center md:min-h-fit
                 text-center rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl
                 before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg dark:before:border-gray-800 dark:before:hover:border-blue-500">
 
