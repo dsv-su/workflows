1ced3046192cbb9c6273736fb1a3142944265404
orkit <ryan@dias.se>
2023-10-17 15:45:16 +0200

    Modified landling page

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -344,6 +344,14 @@
                        Play
                 </span>
             </a>
+            <a href="https://projectproposals.dsv.su.se" class="md:order-8 p-4 relative flex flex-col justify-center items-center md:min-h-fit
+                text-center rounded-xl before:absolute before:inset-0 before:z-10 before:border before:border-gray-200 before:rounded-xl
+                before:transition before:hover:border-2 before:hover:border-blue-600 before:hover:shadow-lg dark:before:border-gray-800 dark:before:hover:border-blue-500">
+
+                <span class="bg-clip-text bg-gradient-to-l from-purple-400 to-blue-600 text-transparent text-2xl font-bold">
+                       Project Proposals
+                </span>
+            </a>
             <!-- end dsvsystem -->
 
         </div>
