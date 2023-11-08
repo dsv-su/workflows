a1080023407edd7001f0ccd12b149e2887c48428
orkit <ryan@dias.se>
2023-11-08 15:20:15 +0100

    Build

--- resources/views/livewire/mobileindicator.blade.php
+++ resources/views/livewire/mobileindicator.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div wire:poll.keep-alive>
     @if(count($dashboard) > 0)
         <span class="md:hidden relative flex h-2 w-2 -mt-3 ml-32">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
