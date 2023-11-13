d25deea9d848aea1051a8d6160f21eeb7033d7ab
orkit <ryan@dias.se>
2023-11-13 11:51:26 +0100

    Fixed desktop view

--- resources/views/review/bar.blade.php
+++ resources/views/review/bar.blade.php
@@ -1,42 +1,54 @@
 <div class="fixed bottom-20 left-0 z-50 w-full md:h-fit bg-white dark:bg-gray-700 dark:border-gray-600">
     <form method="POST" action="{{route('review', $tr)}}">
         @csrf
-        <div class="sm:col-span-4 my-4">
+
+        <div class="sm:hidden sm:col-span-4 my-4">
             <label for="purpose" class="block mb-2 text-sm font-medium text-blue-600 dark:text-white">{{ __("Please Review and Comment") }}</label>
             <textarea id="comment" rows="4"  name="comment"
                       class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-blue-600 focus:ring-primary-500 focus:border-primary-500
                                         dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       placeholder="Please comment the request">
-                        </textarea>
+            </textarea>
         </div>
 
-            <div class="grid h-full max-w-2xl grid-cols-4 bg-white mx-auto font-medium">
-                <a type="button"
-                   href="{{ url()->previous() }}"
-                   class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-blue-500 text-blue-700 font-semibold
-                        hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
-                    <span class="text-sm text-blue-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Cancel and continue later")}}</span>
-                </a>
 
+        <div class="grid h-full max-w-2xl grid-cols-4 bg-white mx-auto font-medium">
 
-                <button type="submit" name="decicion" value="deny"
-                   href=""
-                   class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-red-600 text-red-600 font-semibold
-                        hover:text-white py-2 px-4 border border-red-600 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
-                    <span class="text-sm text-red-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Deny request")}}</span>
-                </button>
-                <button type="submit" name="decicion" value="return"
-                   href=""
-                   class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-yellow-400 font-semibold
-                        hover:text-white py-2 px-4 border border-yellow-400 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
-                    <span class="text-sm text-yellow-400 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Return request")}}</span>
-                </button>
-                <button type="submit" name="decicion" value="approve"
-                        class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-blue-500 text-blue-700 font-semibold
-                        hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
-                    <span class="text-sm text-blue-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Approve request")}}</span>
-                </button>
+            <div class="hidden md:block sm:col-span-4 my-4">
+                <label for="purpose" class="block mb-2 text-sm font-medium text-blue-600 dark:text-white">{{ __("Please Review and Comment") }}</label>
+                <textarea id="comment" rows="4"  name="comment"
+                          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-blue-600 focus:ring-primary-500 focus:border-primary-500
+                                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                          placeholder="Please comment the request">
+            </textarea>
             </div>
+
+            <a type="button"
+               href="{{ url()->previous() }}"
+               class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-blue-500 text-blue-700 font-semibold
+                    hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
+                <span class="text-sm text-blue-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Cancel and continue later")}}</span>
+            </a>
+
+
+            <button type="submit" name="decicion" value="deny"
+               href=""
+               class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-red-600 text-red-600 font-semibold
+                    hover:text-white py-2 px-4 border border-red-600 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
+                <span class="text-sm text-red-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Deny request")}}</span>
+            </button>
+            <button type="submit" name="decicion" value="return"
+               href=""
+               class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-yellow-400 font-semibold
+                    hover:text-white py-2 px-4 border border-yellow-400 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
+                <span class="text-sm text-yellow-400 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Return request")}}</span>
+            </button>
+            <button type="submit" name="decicion" value="approve"
+                    class="inline-flex flex-col mx-2 items-center justify-center px-5 hover:bg-blue-500 text-blue-700 font-semibold
+                    hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded dark:hover:bg-gray-800 group dark:border-gray-600">
+                <span class="text-sm text-blue-700 dark:text-gray-400 group-hover:text-white dark:group-hover:text-blue-500">{{__("Approve request")}}</span>
+            </button>
+        </div>
     </form>
 </div>
 
